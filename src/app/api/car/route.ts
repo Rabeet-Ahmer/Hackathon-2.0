03485208550b8b1987ab759c33cc4d726496c475
Rabeet-Ahmer <rabeetahmer9749@gmail.com>
2025-01-19 23:34:12 +0500

    api-integrated

---
+++ src/app/api/car/route.ts
@@ -0,0 +1,13 @@
+// import { NextResponse } from "next/server";
+
+// export const GET = async () => {
+//     try {
+//       const data = await fetch("");
+//       return NextResponse.json(data, { status: 200 });
+//     } catch (error) {
+//       return NextResponse.json(
+//         { error: "An error occurred while handling the GET request." },
+//         { status: 500 }
+//       );
+//     }
+//   }
